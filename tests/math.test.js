bd57eac58ac57afcfbb080d9cdc150413ed2785b
Vinicius Cosendey <vicosendey@gmail.com>
2020-02-26 12:39:00 -0300

    Jest Writing Tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,13 @@
-test('helloWorld', () => {
+const { calculateTip } = require('../src/math')
+
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+
+    expect(total).toBe(13)
 
 })
 
-test('thisShouldFail', () => {
-    throw new Error('Error')
+test('Should Calculate total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
 })
